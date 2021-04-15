c8c38a343a343ea209a5ad6301f7aea913f10918
Fiona Xie <fifix2001@gmail.com>
2021-04-15 00:57:53 -0400

    linked button

--- hri_proj/main.js
+++ hri_proj/main.js
@@ -3,3 +3,7 @@ $('.search-input').focus(function(){
   }).blur(function(){
     $(this).parent().removeClass('focus');
   })
+
+  document.getElementById("submit-btn").addEventListener("click", function() {
+    window.open("welcome.html");
+  });
