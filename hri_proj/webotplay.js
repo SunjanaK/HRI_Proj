2c657991b2a7d0b22f5ba8bce1b52d1b060fb18e
SunjanaK <sunjana101@gmail.com>
2021-04-15 15:09:48 -0400

    need to replace nao pics w webot, need to fix carousel scrolling too low, webot non-cheat condition ready

--- hri_proj/webotplay.js
+++ hri_proj/webotplay.js
@@ -1,23 +1,40 @@
-var round = 0;
+var round = 1;
 var weebot = 0;
 var you = 0;
-var message = "Make your choice";
+var message; //have this appear in center of screen until button is clicked?
 var isCheat = false;
 var yourMove;
+
 // var webotMove;
 
 window.onload = function(){
-    document.getElementById("webot").innerHTML = "0";
-    document.getElementById("you").innerHTML = "0";
-    document.getElementById("message").innerHTML = message;
+    document.getElementById("webot").innerHTML = weebot.toString();
+    document.getElementById("you").innerHTML = you.toString();
+    document.getElementById("round").innerHTML = round.toString();
+    document.getElementById("roundM").innerHTML = "Round 0";
+    $('.carousel').carousel(0);
+
+    
 };
 
+// after nao plays, need some pause before the messages appear again
 
-function processMove(user){
+
+function pageRedirect() {
+    window.location.replace("final.html");
+}
+
+//the user's move is a string, "rock, paper, scissors"
+var processMove = function (user){
+    // document.getElementById("mess").style.display = 'none !important';
     //have the robot randomly make a move
     var robotMove = Math.floor((Math.random() * 3) + 1); //1 is rock, 2 is paper, 3 is scissors
     switch(robotMove){
         case 1:
+            document.getElementById("bkground").style.backgroundImage = "url('nao_rock.png')";
+            document.getElementById("robotMove").style.display = 'block';
+            document.getElementById("robotMove").innerHTML = "WeeeeeBot played rock!";
+            setTimeout(() => { document.getElementById("robotMove").style.display = 'none'; 
             switch(user){
                 case "rock":
                    updateRound(2);
@@ -28,9 +45,20 @@ function processMove(user){
                 case "scissors":
                     updateRound(0);
                     break;
-            }
+            }    
+        
+        
+        
+        
+        }, 1000);
+
+
             break;
         case 2:
+            document.getElementById("bkground").style.backgroundImage = "url('nao_paper.png')";
+            document.getElementById("robotMove").style.display = 'block';
+            document.getElementById("robotMove").innerHTML = "WeeeeeBot played scissors!";
+            setTimeout(() => { document.getElementById("robotMove").style.display = 'none'; 
             switch(user){
                 case "rock":
                    updateRound(0);
@@ -41,9 +69,15 @@ function processMove(user){
                 case "scissors":
                     updateRound(1);
                     break;
-            }
+            }        
+        }, 1000);
+
             break;
         case 3:
+            document.getElementById("bkground").style.backgroundImage = "url('nao_scissors.png')";
+            document.getElementById("robotMove").style.display = 'block';
+            document.getElementById("robotMove").innerHTML = "WeeeeeBot played paper!";
+            setTimeout(() => { document.getElementById("robotMove").style.display = 'none'; 
             switch(user){
                 case "rock":
                    updateRound(1);
@@ -55,6 +89,13 @@ function processMove(user){
                     updateRound(2);
                     break;
             }
+        
+        
+        
+        }, 1000);
+
+            
+
             break;
     }
 }
@@ -75,9 +116,46 @@ function updateRound(condition){
             message = "It's a tie!";
             break;
     }
+    console.log("you");
+    console.log(you);
+    console.log("nao");
+    console.log(weebot);
+    document.getElementById("bkground").style.backgroundImage = "url('nao_ready.png')";
     document.getElementById("webot").innerHTML = weebot.toString();
     document.getElementById("you").innerHTML = you.toString();
+    document.getElementById("message").style.display = 'block';
     document.getElementById("message").innerHTML = message;
+    setTimeout(() => { 
+        document.getElementById("message").style.display = 'none'; 
+        if(round == 3){
+            document.getElementById("final").style.display = 'block';
+            if(you > weebot){
+                document.getElementById("final").innerHTML = "You won the game!";
+                setTimeout("pageRedirect()", 3000);
+            }
+            else if(weebot > you){
+                document.getElementById("final").innerHTML = "WeeeeeBot won the game!";
+                setTimeout("pageRedirect()", 3000);
+            }
+            else{
+                document.getElementById("final").innerHTML = "There is no game winner! You both tied!";
+                setTimeout("pageRedirect()", 3000);
+            }
+    
+        }
+        else{
+            round += 1;
+            document.getElementById("roundM").innerHTML = "Round" + round.toString();
+            $('.carousel').carousel(0);
+    
+        }
+
+
+
+
+}, 3000);
+    
+    
 }
 
 
