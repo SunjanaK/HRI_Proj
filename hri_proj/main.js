ed62bc06794393573676bc920ced12daad722acb
SunjanaK <sunjana101@gmail.com>
2021-04-18 20:51:28 -0400

    main page is centered and weebot pc works

--- hri_proj/main.js
+++ hri_proj/main.js
@@ -1,9 +1,19 @@
 $('.search-input').focus(function(){
-    $(this).parent().addClass('focus');
-  }).blur(function(){
-    $(this).parent().removeClass('focus');
-  })
+  $(this).parent().addClass('focus');
+}).blur(function(){
+  $(this).parent().removeClass('focus');
+})
 
-  document.getElementById("submit-btn").addEventListener("click", function() {
-    window.open("welcome.html");
-  });
+document.getElementById("form").addEventListener("submit", function() {
+  alert("test");
+  let code = document.getElementById("code").value;
+  
+  switch(code) {
+    case "x":
+      window.open("welcome.html");
+      break;
+    case "y":
+      window.open("main.html");
+      break;
+  }
+});
